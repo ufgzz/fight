47863b4b5ef7027bd876c42b0132b86e50af00e5
zfggithub <948913395@qq.com>
2018-05-07 17:53:30 +0800

    springboot learn by myself
    springboot自学

---
+++ src/main/java/com/oofgz/fight/FightApplication.java
@@ -0,0 +1,12 @@
+package com.oofgz.fight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FightApplication.class, args);
+	}
+}
