2b3b0753632c6f37472a34162e8943c4425ce2f6
zfggithub <948913395@qq.com>
2018-08-30 10:45:11 +0800

    替换Swagger2的默认UI
    替换Swagger2的默认UI

--- src/main/java/com/oofgz/fight/FightSwagger.java
+++ src/main/java/com/oofgz/fight/FightSwagger.java
@@ -6,6 +6,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -28,11 +29,11 @@ public class FightSwagger {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                //.title("Spring Boot中使用Swagger2构建RESTful APIs")
-                //.description("更多SpringBoot 请查看官网")
-                //.termsOfServiceUrl("https://swagger.io/")
-                //.contact(new Contact("程序猿DD", "https://swagger.io/", "zfgican@gmail.com"))
-                //.version("1.0")
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .description("更多SpringBoot 请查看官网")
+                .termsOfServiceUrl("https://swagger.io/")
+                .contact(new Contact("zfgican", "https://swagger.io/", "zfgican@gmail.com"))
+                .version("1.0")
                 .build();
     }
 
