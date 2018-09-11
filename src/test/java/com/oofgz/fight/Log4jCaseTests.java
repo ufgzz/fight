8ee56163392da99be036bb50461e353dd2d02634
zfggithub <948913395@qq.com>
2018-09-12 02:42:31 +0800

    解决依赖冲突
    解决依赖冲突

--- src/test/java/com/oofgz/fight/Log4jCaseTests.java
+++ src/test/java/com/oofgz/fight/Log4jCaseTests.java
@@ -1,7 +1,7 @@
 package com.oofgz.fight;
 
 
-import org.apache.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,15 +10,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Log4j2
 public class Log4jCaseTests {
 
-    private Logger logger = Logger.getLogger(getClass());
 
     @Test
     public void log4jTest() throws Exception {
-        logger.info("输出info");
-        logger.debug("输出debug");
-        logger.error("输出error");
+        log.info("输出info");
+        //log.debug("输出debug");
+        //log.error("输出error");
     }
 
 
