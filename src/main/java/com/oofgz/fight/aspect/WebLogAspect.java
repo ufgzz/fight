2d0c582b81bae1114af4fd62aea68f099bad54bc
zfggithub <948913395@qq.com>
2018-09-13 00:02:46 +0800

    添加Restful Controller请求的
    添加Restful Controller请求的

---
+++ src/main/java/com/oofgz/fight/aspect/WebLogAspect.java
@@ -0,0 +1,64 @@
+package com.oofgz.fight.aspect;
+
+import lombok.extern.log4j.Log4j2;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+@Aspect
+@Order(5)
+@Log4j2
+@Component
+public class WebLogAspect {
+
+
+    private ThreadLocal<Long> startTime = new ThreadLocal<>();
+
+    @Pointcut("execution(public * com.oofgz.fight.controller..*.*(..))")
+    public void webLog(){}
+
+    /**
+     * 在切入点前的操作，按order的值由小到大执行
+     * @param joinPoint
+     */
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint) {
+        startTime.set(System.currentTimeMillis());
+
+        // 接收到请求，记录请求内容
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        // 记录下请求内容
+        log.info("URL : " + request.getRequestURL().toString());
+        log.info("HTTP_METHOD : " + request.getMethod());
+        log.info("IP : " + request.getRemoteAddr());
+        log.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        log.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+
+    }
+
+    /**
+     * 在切入点后的操作，按order的值由大到小执行
+     * @param ret
+     */
+    @AfterReturning(returning = "ret", pointcut = "webLog()")
+    public void doAfterReturning(Object ret) {
+        // 处理完请求，返回内容
+        log.info("RESPONSE : " + ret);
+        log.info("SPEND TIME : " + (System.currentTimeMillis() - startTime.get()));
+    }
+
+
+
+
+}
